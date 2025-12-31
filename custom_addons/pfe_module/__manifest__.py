18c36f00b22387d1500617e66dc4db26ee83d574
Sahara hammami <saharhammami487@gmail.com>
2025-12-31 21:22:24 +0100

    chech

--- custom_addons/pfe_module/__manifest__.py
+++ custom_addons/pfe_module/__manifest__.py
@@ -4,6 +4,8 @@
     'author': 'PFE Student',
     'category': 'Technical',
     'depends': ['base'],
+    'data': [],
     'installable': True,
-    'application': True,
+    'application': True, # This makes it show up in the main Apps list
+    'auto_install': False,
 }
